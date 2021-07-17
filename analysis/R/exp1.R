20c2017d206d071223d3070c97017ce1314a0df2
Satoru Nishiyama <nishi.satoru.yama0226@gmail.com>
2021-07-18 00:16:32 +0900

    update R code

--- analysis/R/exp1.R
+++ analysis/R/exp1.R
@@ -1,4 +1,5 @@
 # setup -------------------------------------------------------------------
+
 if(!require(pacman)) install.packages("pacman")
 pacman::p_load(tidyverse, here, jsonlite)
 
@@ -6,7 +7,34 @@ pacman::p_load(tidyverse, here, jsonlite)
 # data import -------------------------------------------------------------
 
 df_e1 <- fs::dir_ls(path = here::here("exp/data/exp1"), glob = "*.csv") %>% 
-  purrr::map_dfr(readr::read_csv)
+  purrr::map_dfr(readr::read_csv, na = c("", "NA", "null"))
+
+
+# demography --------------------------------------------------------------
+
+df_subj <- df_e1 %>% 
+  dplyr::distinct(tempID, age, sex)
+
+# analyze the data as you like
+
+# flanker -----------------------------------------------------------------
+
+df_flanker <- df_e1 %>% 
+  dplyr::filter(task == "flanker_main") %>% 
+  dplyr::select(tempID, condition, rt, correct)
+
+# analyze the data as you like
+# about functions below, see https://blog.atusy.net/2018/11/09/binwdith-for-geom-histogram/
+bw = function(x, f) ceiling((max(x) - min(x)) / f(x))
+scott = function(x) bw(x, nclass.scott)
+df_flanker %>% 
+  dplyr::mutate(cond = dplyr::recode(condition, 一致 = "cong", 中性 = "neu", 不一致 = "incong")) %>% 
+  ggplot(aes(x = rt)) +
+  geom_histogram(stat = "bin", binwidth = scott) +
+  facet_grid(rows = vars(cond), cols = vars(correct))
+
+
+# BIS/BAS -----------------------------------------------------------------
 
 df_ques <- df_e1 %>% 
   dplyr::filter(task == 'bis_bas') %>% 
@@ -18,14 +46,16 @@ df_ques <- df_e1 %>%
                       names_pattern = "Q([0-9]+)",
                       names_transform = list(q_id = as.numeric)) 
 
-
 df_items <- list(bis = c(13, 10, 15, 6, 20, 18, 1),
                    drive = c(7, 2, 9, 17),
                    responsive = c(11, 19, 5, 14, 3),
                    seek = c(8, 12, 4, 16)) %>% 
-  purrr::map(~data.frame(q_id = .x - 1)) %>% 
+  purrr::map(~data.frame(q_id = .x)) %>% 
   dplyr::bind_rows(.id = "fct_nm")
 
-dplyr::left_join(df_ques, df_items, by = "q_id") %>% 
+df_bisbas <- dplyr::left_join(df_ques, df_items, by = "q_id") 
+
+# analyze the data as you like
+df_bisbas %>% 
   dplyr::group_by(tempID, fct_nm) %>% 
-  dplyr::summarise(score = sum(value))
+  dplyr::summarise(score = mean(value))
